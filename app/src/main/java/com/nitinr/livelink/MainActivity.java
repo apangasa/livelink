0f1eab156c3a312cebf6d624e4fcafdb19732b16
nitinramadoss <nitinramadoss@gmail.com>
2021-03-23 13:01:41 -0400

    removes unused code

--- app/src/main/java/com/nitinr/livelink/MainActivity.java
+++ app/src/main/java/com/nitinr/livelink/MainActivity.java
@@ -5,26 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
-import com.google.ar.core.Anchor;
-import com.google.ar.core.ArCoreApk;
-import com.google.ar.core.Config;
-import com.google.ar.core.Pose;
-import com.google.ar.core.Session;
-import com.google.ar.core.exceptions.UnavailableApkTooOldException;
-import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
-import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
-import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
-import com.google.ar.sceneform.AnchorNode;
+
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.Vector3;
-import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
@@ -37,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     private static final double MIN_OPENGL_VERSION = 3.0;
 
     private ArFragment arFragment;
-    private ModelRenderable andyRenderable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,25 +33,19 @@ public class MainActivity extends AppCompatActivity {
         if (!checkIsSupportedDeviceOrFinish(this)) {
             Toast.makeText(this, "ARCore Scene Form not supported", Toast.LENGTH_LONG)
                     .show();
-        } else {
-            Toast.makeText(this, "ARCore Scene Form supported!", Toast.LENGTH_LONG)
-                    .show();
         }
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
+        assert arFragment != null;
         arFragment.getArSceneView().getPlaneRenderer().setEnabled(false);
 
-        //arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> createRenderable(hitResult.createAnchor()));
-
         createRenderable();
-
-        //createSession();
     }
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
         String openGlVersionString =
-                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)))
                         .getDeviceConfigurationInfo()
                         .getGlEsVersion();
         if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
@@ -82,28 +62,10 @@ public class MainActivity extends AppCompatActivity {
         ViewRenderable.builder()
                 .setView(this, R.layout.activity_renderable)
                 .build()
-                .thenAccept(viewRenderable -> {
-                    addToScene(viewRenderable);
-                });
+                .thenAccept(this::addToScene);
     }
 
     private void addToScene(ViewRenderable viewRenderable) {
-//        //Session session = arFragment.getArSceneView().getSession();
-//        float[] pos = { 0, 0, -1 };
-//        float[] rotation = { 0, 0, 0, 1 };
-//        //Anchor anchor =  session.createAnchor(new Pose(pos, rotation));
-//
-//        //assert session != null;
-////        Anchor anchor = session.createAnchor(Objects.requireNonNull(arFragment.getArSceneView()
-////                .getArFrame()).getCamera().getDisplayOrientedPose()
-////                .compose(Pose.makeTranslation(0, 0, -1f))
-////                .extractTranslation());
-//        AnchorNode anchorNode = new AnchorNode(anchor);
-//        anchorNode.setRenderable(viewRenderable);
-//        anchorNode.setParent(arFragment.getArSceneView().getScene());
-//
-//        arFragment.getArSceneView().getScene().addChild(anchorNode);
-
         Vector3 forward = arFragment.getArSceneView().getScene().getCamera().getForward();
         Vector3 worldPosition = arFragment.getArSceneView().getScene().getCamera().getWorldPosition();
         Vector3 position = Vector3.add(forward, worldPosition);
