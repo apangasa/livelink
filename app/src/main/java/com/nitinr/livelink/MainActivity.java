fd70934c85980f32410f77075d8fe5e6c2337850
nitinramadoss <nitinramadoss@gmail.com>
2021-03-23 20:57:42 -0400

    tests ar fragment screenshot to base64

--- app/src/main/java/com/nitinr/livelink/MainActivity.java
+++ app/src/main/java/com/nitinr/livelink/MainActivity.java
@@ -5,22 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private static final double MIN_OPENGL_VERSION = 3.0;
 
     private ArFragment arFragment;
+    private ViewRenderable viewRenderable;
+    private boolean snapShotTaken = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,21 +58,9 @@ public class MainActivity extends AppCompatActivity {
         assert arFragment != null;
         arFragment.getArSceneView().getPlaneRenderer().setEnabled(false);
 
-//        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        View vi = inflater.inflate(R.layout.activity_renderable, null);
-//
-//        TextView nameView = (TextView)vi.findViewById(R.id.name);
-//        nameView.setText("hello");
-
         createRenderable();
 
-//        final Handler handler = new Handler(Looper.getMainLooper());
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                createRenderable();
-//            }
-//        }, 5000);
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onSceneUpdate);
     }
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
@@ -85,8 +86,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addToScene(ViewRenderable viewRenderable) {
-//        TextView tv = viewRenderable.getView().findViewById(R.id.name);
-//        tv.setText("Testing");
+        this.viewRenderable = viewRenderable;
 
         Vector3 forward = arFragment.getArSceneView().getScene().getCamera().getForward();
         Vector3 worldPosition = arFragment.getArSceneView().getScene().getCamera().getWorldPosition();
@@ -103,6 +103,57 @@ public class MainActivity extends AppCompatActivity {
         Node render = new Node();
         render.setParent(node);
         render.setRenderable(viewRenderable);
+    }
+
+    private void snapShot() {
+        try {
+            Image snapShot = Objects.requireNonNull(arFragment.getArSceneView().getArFrame()).acquireCameraImage();
+            ByteBuffer byteBufferY = snapShot.getPlanes()[0].getBuffer();
+            ByteBuffer byteBufferU = snapShot.getPlanes()[1].getBuffer();
+            ByteBuffer byteBufferV = snapShot.getPlanes()[2].getBuffer();
+
+            byte[] bytes = new byte[byteBufferY.remaining() + byteBufferU.remaining() + byteBufferV.remaining()];
+            byteBufferY.get(bytes, 0, byteBufferY.remaining());
+            byteBufferU.get(bytes, byteBufferY.remaining(), byteBufferV.remaining());
+            byteBufferV.get(bytes, byteBufferY.remaining() + byteBufferV.remaining(), byteBufferU.remaining());
+
+
+            String base64 = Base64.encodeToString(bytes, Base64.DEFAULT);
+//            Log.d(TAG, "START");
+//            Log.d(TAG, base64);
+//            Log.d(TAG, "END");
+
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
+//            YuvImage yuv = new YuvImage(bytes, ImageFormat.YUV_420_888, snapShot.getWidth(), snapShot.getHeight(), null);
+//            yuv.compressToJpeg(new Rect(0, 0, snapShot.getWidth(), snapShot.getHeight()), 100, out);
+//
+//            byte[] imageBytes = out.toByteArray();
+//            Bitmap bitmapImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+//            Bitmap bitmapImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+//
+//            assert viewRenderable != null;
+//
+//            ImageView iv = viewRenderable.getView().findViewById(R.id.imageView);
+//            //iv.setImageBitmap(bitmapImage);
+//            iv.setImageResource(R.drawable.ic_launcher_foreground);
+            snapShotTaken = true;
+            snapShot.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void onSceneUpdate(FrameTime frameTime) {
+        if (!snapShotTaken) {
+            final Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    snapShot();
+                }
+            }, 5000);
+        }
     }
 
     @Override
