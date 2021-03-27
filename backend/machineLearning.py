c992a44737d337679619544b7c1cdfd57d19262b
apangasa <60531810+apangasa@users.noreply.github.com>
2021-03-27 15:09:40 -0400

    Manage upload/download of features and recognize new images

--- backend/machineLearning.py
+++ backend/machineLearning.py
@@ -1,12 +1,12 @@
 import faceRecog
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
 from google.cloud import storage
 import logging
 
 
-def recognize(base64string):
-    faceRecog.get_landmarks(base64string, rotate=True)
-    return 0
+def get_features(base64string):
+    return faceRecog.get_landmarks(base64string)
 
 
 def serialize_matrix(matrix):
@@ -46,6 +46,16 @@ def upload_class_file(new_class_arr):
     blob.upload_from_string(string)
 
 
+def upload_feature_file(new_feature_arr):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('live-link-308404.appspot.com')
+    blob = bucket.blob('/features')
+
+    string = serialize_matrix(new_feature_arr)
+
+    blob.upload_from_string(string)
+
+
 def download_class_file():
     storage_client = storage.Client()
     bucket = storage_client.bucket('live-link-308404.appspot.com')
@@ -53,6 +63,15 @@ def download_class_file():
 
     string = blob.download_as_string().decode('utf-8')
     return deserialize_string(string, 2)
+
+
+def download_feature_file():
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('live-link-308404.appspot.com')
+    blob = bucket.blob('/features')
+
+    string = blob.download_as_string().decode('utf-8')
+    return deserialize_string(string, 468 * 3 + 1)
 
 
 def get_next_class():
@@ -72,6 +91,40 @@ def insert_new_class(class_id, person_id):
     new_class_arr = np.vstack(class_arr, insert_arr)
 
     upload_class_file(new_class_arr)
+
+
+def create_instance(base64string, class_id):
+    features = get_features(base64string)
+    class_arr = np.array([class_id])
+    full_arr = np.hstack(features, class_arr)
+
+    old_features = download_feature_file()
+    new_features = np.vstack(old_features, full_arr)
+
+    upload_feature_file(new_features)
+
+
+def recognize(base64string):
+    landmarks = faceRecog.get_landmarks(base64string, rotate=True)
+    full_features = download_feature_file()
+    features = full_features[:, :-1]
+    classes = full_features[:, -1]
+
+    classifier = KNeighborsClassifier(n_neighbors=1)
+    classifier.fit(features, classes)
+
+    predictions = classifier.predict([landmarks])
+    prediction, = predictions
+
+    return prediction
+
+
+def get_id(class_num):
+    class_arr = download_class_file()
+    for row in class_arr:
+        if row[0] == class_num:
+            return row[1]
+    return 0
 
 
 def test_upload():
